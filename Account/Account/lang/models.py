b008ea67f375097b66956c2ca5ed2e3458f45076
Bratslav <bratslav1776@gmail.com>
2024-03-24 17:41:17 +0200

    Edited app Lang 22/03/2024

--- Account/Account/lang/models.py
+++ Account/Account/lang/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+
 
 class Langv(models.Model):
     title = models.CharField(max_length=255, verbose_name='Язык интерфейса')
@@ -11,9 +13,24 @@ class Langv(models.Model):
     def get_absolute_url(self):
         return reverse('lang', kwargs={'slug': self.slug})
 
-
-
     class Meta:
         verbose_name = 'Язык'
         verbose_name_plural = 'Языки'
         ordering = ['title']
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birth_date = models.DateField(null=True, blank=True)
+    language = models.ForeignKey(Langv, on_delete=models.PROTECT)
+    description = models.TextField(null=True, blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
